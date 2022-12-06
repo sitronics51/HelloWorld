778747b84201cc83614a25d7007c3efad0ec0735
Gheorghe Siminițchi <34488513+sitronics51@users.noreply.github.com>
2022-12-06 10:09:24 +0200

    Sprint 1.1

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /*String name = "Jony!";
+        System.out.println("Hello, " + name);// Metoda de citire dintr-o variabilă
+        */
+        Scanner scanner1 = new Scanner(System.in);
+        String name;
+
+        System.out.println("Enter your first and last name:");
+        name = scanner1.nextLine();
+        System.out.print("Hello, " + name);
+    }
+}
